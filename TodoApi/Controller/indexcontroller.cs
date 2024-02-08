acde4ca97c7c8322e856ef3deb7834f456b98fe3
ramik-shahi <118872289+ramik-shahi@users.noreply.github.com>
2024-02-08 23:44:09 +0545

    Add files via upload

---
+++ TodoApi/Controller/indexcontroller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Model;
+
+namespace TodoApi.Controller
+{
+   
+    public class indexcontroller : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("/demo")]
+        public ActionResult<string> Get()
+        {
+            var demoInstance = new TodoItem();
+            demoInstance.main(null); // You might want to pass appropriate arguments
+
+            return "Hello from DemoController!";
+        }
+
+
+    }
+}
---
+++ TodoApi/Model/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TodoApi.Model
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
